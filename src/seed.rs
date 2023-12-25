23cc816e76fc652cf56e6ebb92682ca38774f333
Juan Pablo Darago <jpdarago@gmail.com>
2023-12-25 01:07:44 -0300

    Add an autocompleter

--- src/seed.rs
+++ src/seed.rs
@@ -3,10 +3,12 @@ use crate::types::*;
 use anyhow::bail;
 use bip39::{Language, Mnemonic};
 use hmac::Mac;
+use inquire::autocompletion::{Autocomplete, Replacement};
 use inquire::Text;
+use lazy_static::lazy_static;
 use pbkdf2::password_hash::{PasswordHasher, SaltString};
 use std::fmt;
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
 type HmacSha512 = hmac::Hmac<sha2::Sha512>;
 
@@ -72,16 +74,79 @@ pub fn decode_phrase(seed_format: &SeedFormat, phrase: &str) -> Result<Vec<u8>> 
     }
 }
 
+#[derive(Clone, Default)]
+struct Completer {
+    words: Vec<String>,
+}
+
+impl Completer {
+    fn new() -> Result<Completer> {
+        let wordlist_filepath: std::path::PathBuf =
+            [env!("CARGO_MANIFEST_DIR"), "resources/bip39.txt"]
+                .iter()
+                .collect();
+        let wordfile = std::fs::File::open(wordlist_filepath)?;
+        let mut words: Vec<String> = Vec::new();
+        for line in std::io::BufReader::new(wordfile).lines() {
+            words.push(line?.trim().to_string());
+        }
+        Ok(Completer { words })
+    }
+}
+
+lazy_static! {
+    static ref COMPLETER: Completer = Completer::new().expect("Failed to read wordlist");
+}
+
+impl Autocomplete for Completer {
+    fn get_completion(
+        &mut self,
+        input: &str,
+        highlighted_suggestion: Option<String>,
+    ) -> std::result::Result<
+        Option<std::string::String>,
+        Box<(dyn std::error::Error + Send + Sync + 'static)>,
+    > {
+        Ok(match highlighted_suggestion {
+            Some(suggestion) => Replacement::Some(suggestion),
+            None => Replacement::Some(input.to_string()),
+        })
+    }
+
+    fn get_suggestions(
+        &mut self,
+        input: &str,
+    ) -> std::result::Result<
+        Vec<std::string::String>,
+        Box<(dyn std::error::Error + Send + Sync + 'static)>,
+    > {
+        Ok(self
+            .words
+            .iter()
+            .filter(|s| s.to_lowercase().starts_with(&input))
+            .map(|s| String::from(s))
+            .collect())
+    }
+}
+
 pub fn from_prompt(seed_format: &SeedFormat) -> Result<Vec<u8>> {
+    Text::new(&format!("Please input a {} phrase: ", seed_format));
     loop {
-        let input = Text::new(&format!("Please input a {} phrase: ", seed_format)).prompt()?;
-        let words: String = input.split_whitespace().collect::<Vec<&str>>().join(" ");
-        match decode_phrase(seed_format, &words) {
+        let mut result = vec![];
+        for i in 0..12 {
+            let input = Text::new(&format!("Word {}: ", i + 1))
+                .with_autocomplete(Completer::new()?)
+                .prompt()?;
+            let word: &str = input.trim();
+            result.push(word.to_string());
+        }
+        match decode_phrase(seed_format, &result.join(" ")) {
             Ok(phrase) => {
                 return Ok(phrase);
             }
             Err(s) => {
                 Text::new(&format!("Failed to parse {} phrase: {}", seed_format, s));
+                result.clear();
                 io::stdout().flush().unwrap();
             }
         }
